508996c969beaa8c9c2a38ad0d56ff9f816ab42f
Sopyer <Sopyer@cedf5d71-8d09-5b43-86cc-2477a3c2614d>
2007-05-09 11:33:23 +0000

    git-svn-id: file:///host/dev/_Previous_/Diploma/Repository@14 cedf5d71-8d09-5b43-86cc-2477a3c2614d

--- Infinity/glClasses/glTexture.h
+++ Infinity/glClasses/glTexture.h
@@ -1,6 +1,23 @@
+/**
+ * \ingroup glClasses
+ * Клас glTexture.
+ * Цей клас є базовим для представлення текстур
+ * та містить їх характеристики: тип, дескриптор,
+ * опис внутрішнього представлення, а також задає
+ * атоматичну генерацію mipmap рівнів та параметри 
+ * фільтрування.
+**/
 class glTexture
 {
 	public:
+		/**
+		 * Конструктор.
+		 * \param target тип текстури, як це вказується в OpenGL
+		 * \param internalFormat задає формат внутрішнього представлення зображення
+		 * \param minFilter задає фільтр, що використовується при зменшенні
+		 * \param magFilter задає фільтр, що використовується при збільшенні
+		 * \param forceMipmap вказує, чи генерувати примусово рівні деталізації
+		**/
 		glTexture(GLenum target, GLint internalFmt, GLint minFilter, GLint magFilter, bool forceMipmap=true)
 		{
 			internalFormat_ = internalFmt;
@@ -12,20 +29,51 @@ class glTexture
 			glTexParameteri(target, GL_TEXTURE_MAG_FILTER, magFilter);
 
 		}
+		/**
+		 * Деструктор
+		**/
 		~glTexture(void){glDeleteTextures(1, &handle_);}
 		
 	public:
+		/**
+		 * Тип текстури.
+		**/
 		GLenum	target_;
+		/**
+		 * Дескриптор текстури.
+		**/
 		GLuint	handle_;
+		/**
+		 * Внутрішній формат текстури.
+		**/
 		GLint	internalFormat_;
 };
 
+/**
+ * \ingroup glClasses
+ * Клас двовимірної текстури.
+ * Містить методи для роботи з цим типом текстур:
+ * Встановлення параметрів розширення координат, а
+ * також зображення текстури.
+**/
 class glTexture2D: public glTexture
 {
 	public:
+		/**
+		 * Конструктор.
+		 * \param internalFormat задає формат внутрішнього представлення зображення
+		 * \param minFilter задає фільтр, що використовується при зменшенні
+		 * \param magFilter задає фільтр, що використовується при збільшенні
+		 * \param forceMipmap вказує, чи генерувати примусово рівні деталізації
+		**/
 		glTexture2D(GLint internalFmt, GLint minFilter, GLint magFilter, bool forceMipmap=true):
 		  glTexture(GL_TEXTURE_2D, internalFmt, minFilter, magFilter, forceMipmap){}
 
+		/**
+		 * Задає спосіб розширення текстурних координат за межі [0..1]
+		 * \param wrapS розширення по осі S(горизонтальна)
+		 * \param wrapT розширення по осі T(вертикальна)
+		**/
 		void setWrapMode(GLint wrapS, GLint wrapT)
 		{
 			glBindTexture(target_, handle_);
@@ -33,6 +81,14 @@ class glTexture2D: public glTexture
 			glTexParameteri(target_, GL_TEXTURE_WRAP_T, wrapT);
 		}
 
+		/**
+		 * Задає зображення текстури
+		 * \param width ширина зображення
+		 * \param height висота зображення
+		 * \param format формат зображення - тип та кількість каналів
+		 * \param type тип елементу каналу
+		 * \param pixels вказівник на дані
+		**/
 		void setImage2D(GLsizei width, GLsizei height, GLenum format, GLenum type, const GLvoid *pixels)//maybe this is not usefullas
 		{
 			glBindTexture(target_, handle_);
