788e168cda29ff030fb6c41a812beb7925cd56ab
Mykhailo Parfeniuk <sopyer@gmail.com>
2013-11-08 22:23:13 +0200

    Fixed tests
    * fixed solution and upgraded to MSVS2010
    * removed gtest library, switched to sput

--- Tests/VGTests/main.cpp
+++ Tests/VGTests/main.cpp
@@ -1,7 +1,7 @@
-#include <gtest/gtest.h>
+#include <sput.h>
 #include "impl/Math.h"
 
-TEST(CubicTCGenTest, OrientSelectionBug)
+void test_orientation_selection_bug()
 {
 	Array<glm::vec2> cp;
 	cp.push_back(glm::vec2(12.52016f, 0.19535054f));
@@ -13,20 +13,26 @@ TEST(CubicTCGenTest, OrientSelectionBug)
 	Array<glm::vec3> tc;
 
 	cubicTriVertices(cp, p, tc);
-	EXPECT_TRUE(cubic::calcImplicit(glm::mix(tc[0], tc[3], 0.5f))<=0);
+	sput_fail_unless(cubic::calcImplicit(glm::mix(tc[0], tc[3], 0.5f))<=0, "Check midpoint explicit cubic function sign");
 
 	float	s0 = cubic::calcImplicit(tc[0]),
 			s1 = cubic::calcImplicit(tc[1]),
 			s2 = cubic::calcImplicit(tc[2]),
 			s3 = cubic::calcImplicit(tc[3]);
 
-	EXPECT_TRUE(ml::greaterThenE(s1, 0) || ml::equalE(s1, 0));
-	EXPECT_TRUE(ml::greaterThenE(s2, 0) || ml::equalE(s2, 0));
-	EXPECT_TRUE(ml::equalE(s0, 0));
-	EXPECT_TRUE(ml::equalE(s3, 0));
+	sput_fail_unless(ml::greaterThenE(s1, 0) || ml::equalE(s1, 0), "Check hull point 1 explicit cubic function sign");
+	sput_fail_unless(ml::greaterThenE(s2, 0) || ml::equalE(s2, 0), "Check hull point 2 explicit cubic function sign");
+	sput_fail_unless(ml::equalE(s0, 0), "Check hull point 0 explicit cubic function sign");
+	sput_fail_unless(ml::equalE(s3, 0), "Check hull point 3 explicit cubic function sign");
 }
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    sput_start_testing();
+
+    sput_enter_suite("VG: cubic geometry generation tests");
+    sput_run_test(test_orientation_selection_bug);
+
+    sput_finish_testing();
+
+    return sput_get_return_value();
 }
